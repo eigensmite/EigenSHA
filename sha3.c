ae08f1f6699b62be4e89e1139da547fc8fa238e9
eigensmite <stanleyjohn@ksu.edu>
2026-03-13 21:44:59 -0500

    simplified keccak-p implementation to only use keccak-f specifications, rewrote how state is accessed in keccak function to allow A to be defined on the same memory as state

--- sha3.c
+++ sha3.c
@@ -16,7 +16,7 @@ void print_lanes(uint64_t A[5][5]) {
     printf("Xor'd state (as lanes of integers)\n");
     for (int y = 0; y < 5; y++) {
         for(int x = 0; x < 5; x++) {
-            printf("\t[%d, %d] = %016lX\n", x, y, A[x][y]);
+            printf("\t[%d, %d] = %016lX\n", y, x, A[y][x]);
         }
     }
 }
@@ -26,7 +26,7 @@ void print_state(uint64_t A[5][5]) {
     for (int y = 0; y < 5; y++) {
         for (int x = 0; x < 5; x++) {
             for (int z = 0; z < 64; z+=8) {
-                printf("%02lX ", (A[x][y] >> z) & 0xff);
+                printf("%02lX ", (A[y][x] >> z) & 0xff);
             }
             if ((y+x) % 2 == 1) printf("\n\t");
         }
@@ -38,43 +38,38 @@ void print_state(uint64_t A[5][5]) {
 typedef size_t(*pad_fn)(mpz_t, const mpz_t, const size_t);
 typedef void(*perm_fn)(mpz_t, const mpz_t);
 
-
 const static int OFFSET_TABLE[5][5] = {
-    {   0,  36,   3, 105, 210 },
-    {   1, 300,  10,  45,  66 },
-    { 190,   6, 171,  15, 253 },
-    {  28,  55, 153,  21, 120 },
-    {  91, 276, 231, 136,  78 }
+    {  0,  1, 62, 28, 27 },
+    { 36, 44,  6, 55, 20 },
+    {  3, 10, 43, 25, 39 },
+    { 41, 45, 15, 21,  8 },
+    { 18,  2, 61, 56, 14 }
 };
 
-const static uint64_t RC_LFSR[4] = {0xeabe509fe178d01,0xa452a7767bf4cd46,0xb0fb7ae886c79cc5,0x8e25c0c93720adac};
+const static uint64_t RC[24] = { 
+    0x0000000000000001, 0x0000000000008082, 0x800000000000808A, 0x8000000080008000, 0x000000000000808B, 0x0000000080000001, 0x8000000080008081, 0x8000000000008009, 
+    0x000000000000008A, 0x0000000000000088, 0x0000000080008009, 0x000000008000000A, 0x000000008000808B, 0x800000000000008B, 0x8000000000008089, 0x8000000000008003, 
+    0x8000000000008002, 0x8000000000000080, 0x000000000000800A, 0x800000008000000A, 0x8000000080008081, 0x8000000000008080, 0x0000000080000001, 0x8000000080008008
+};
 
-static inline void _theta(uint64_t A[5][5], const size_t w) {
+static inline void _theta(uint64_t A[5][5]) {
     uint64_t C[5] = {0};
-    for (int x = 0; x < 5; x++) C[x] = A[x][0] ^ A[x][1] ^ A[x][2] ^ A[x][3] ^ A[x][4];
-    for (int y = 0; y < 5; y++) 
-        for (int x = 0; x < 5; x++)
-            A[x][y] ^= C[(x+4)%5] ^ ((C[(x+1)%5] >> ((w-1)%w)) | C[(x+1)%5] << (1%w));
+    for (int x = 0; x < 5; x++) C[x] = A[0][x] ^ A[1][x] ^ A[2][x] ^ A[3][x] ^ A[4][x];
+    for (int y = 0; y < 5; y++) for (int x = 0; x < 5; x++)
+        A[y][x] ^= C[(x+4)%5] ^ ((C[(x+1)%5] >> 63) | (C[(x+1)%5] << 1));
 }
 
-static inline void _rho(uint64_t A[5][5], const size_t w) {
-    int offset;
-    for (int x = 0; x < 5; x++) {
-        for (int y = 0; y < 5; y++) {
-            if((offset = OFFSET_TABLE[x][y] % w) != 0) {
-                A[x][y] = (A[x][y] << offset) | (A[x][y] >> (w - offset));
-                if(w<64) A[x][y] &= (1ULL << w) - 1;
-            }
-        }
-    }
+static inline void _rho(uint64_t A[5][5]) {
+    for (int x = 0; x < 5; x++) for (int y = 0; y < 5; y++) 
+        A[y][x] = (A[y][x] << OFFSET_TABLE[y][x]) | (A[y][x] >> (64 - OFFSET_TABLE[y][x]));     
 }
 
 static inline void _pi(uint64_t A[5][5]) {
-    uint64_t prev = A[1][0], temp;
+    uint64_t prev = A[0][1], temp;
     int prev_x = 1, prev_y = 0;
     for (int t = 0; t < 24; t++) {
         int x = prev_y, y = (2 * prev_x + 3 * prev_y) % 5;
-        temp = A[x][y], A[x][y] = prev, prev = temp;
+        temp = A[y][x], A[y][x] = prev, prev = temp;
         prev_x = x, prev_y = y;
     }
 }
@@ -82,83 +77,51 @@ static inline void _pi(uint64_t A[5][5]) {
 static inline void _chi(uint64_t A[5][5]) {
     uint64_t R[5];
     for (int y = 0; y < 5; y++) {
-        for (int x = 0; x < 5; x++) R[x] = A[x][y];
-        for (int x = 0; x < 5; x++) A[x][y] = R[x] ^ ((~R[(x+1)%5]) & R[(x+2)%5]);
+        for (int x = 0; x < 5; x++) R[x] = A[y][x];
+        for (int x = 0; x < 5; x++) A[y][x] = R[x] ^ ((~R[(x+1)%5]) & R[(x+2)%5]);
     }
 }
 
-static inline void _iota(uint64_t A[5][5], const int i, const int l, const size_t w) {
-    uint64_t RC = 0;
-    for (int j = 0; j <= l; j++) {
-        RC |= (((RC_LFSR[((j+7*i)%255)/64] >> (((j+7*i)%255)%64)) & 1) << ((1 << j) - 1));
-    }
-    A[0][0] ^= RC;
-}
 
-static inline void keccak_p(uint64_t state[25], const size_t b, const size_t n_r) {
-    switch (b) {
-        case 1600: case 800: case 400: case 200: case 100: case 50: case 25: break;
-        default: 
-            printf("keccak_p requires bit width b of size { 1600, 800, 400, 200, 100, 50, 25 }; you have b=%lu\n", b);
-            return;
-    }
-
-    const size_t w = b/25;
-    const size_t l = (size_t) log2(w);
-
-    uint64_t A[5][5] = {{0}};
-
-    for (int y = 0; y < 5; y++) {
-        for (int x = 0; x < 5; x++) {
-            if(w==64) A[x][y] = state[5*y + x];
-            else A[x][y] = (state[w*(5*y+x)/64] >> ((w*(5*y+x))%64)) & ((1ULL << w) - 1);
-        }
-    }
-
-    //print_state(A);
-
-    for(int i = 12+2*l-n_r; i < 12+2*l; i++) {
-        _theta(A, w);          // 𝜃
-          _rho(A, w);         // 𝜌
-           _pi(A);           // 𝜋
-          _chi(A);          // 𝜒
-        _iota(A, i, l, w); // 𝜄
-    }
-
-    for(int i = 0; i < (25 + 64/w - 1)*w/64; i+=1) {
-        if(w==64) state[i] = A[i%5][i/5];
-        else {
-            for (int j = 1; j <= 64/w; j++) {
-                state[i] <<= w;
-                if((64/w-j+64/w*i)/(5) != 5) state[i] += A[(64/w-j+64/w*i)%5][(64/w-j+64/w*i)/(5)];
-            }
-        }
-    }
+static inline void _iota(uint64_t A[5][5], const int i) {
+    A[0][0] ^= RC[i];
 }
 
 static inline void keccak_f(uint64_t state[25]) {
-    keccak_p(state, 1600, 24);
+    uint64_t (*A)[5] = (uint64_t (*)[5])state;
+    for(int i = 0; i < 24; i++) {
+        _theta(A);        // 𝜃
+          _rho(A);       // 𝜌
+           _pi(A);      // 𝜋
+          _chi(A);     // 𝜒
+        _iota(A, i);  // 𝜄
+    }
 }
 
 // r is always in bits
 static void sponge_absorb_bytes(uint64_t state[25], uint8_t buffer[4096], size_t *preloaded_bytes, const size_t buffer_len, const struct SHA_3 sha){
     const size_t r = sha.r;
-    if(r%8 != 0) {
-        printf("r must be divisible by 8 because we are reversing endienness of bytes\n");
-        assert(r%8 == 0);
-    }
-
-    //printf("%ld\n", buffer_len);
 
     uint8_t *buffer_start = buffer;
 
     size_t bytes_remain = buffer_len - (buffer - buffer_start);
 
     while (bytes_remain + *preloaded_bytes >= r/8) {
-        for (int i = *preloaded_bytes; i < r/8; i++) {
+        #pragma region start
+        #pragma endregion
+        
+        int i;
+        // for (i = *preloaded_bytes; i < r/8 && i % 8 == 0; i+=8) {
+        //     state[*preloaded_bytes/8] ^= ((uint64_t *)buffer)[0];
+        //     buffer += 8;
+        // } 
+        // *preloaded_bytes += i;
+
+        for (i = *preloaded_bytes; i < r/8; i++) {
             state[i/8] ^= ((uint64_t) buffer[0]) << (8*(i%8));
             buffer++;
         }
+
         keccak_f(state);
         *preloaded_bytes = 0;
 
